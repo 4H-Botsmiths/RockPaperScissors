5611e2dce77102c13081e0c15e2320c0aa624700
Kolton Bloom <bloomkd46@icloud.com>
2024-03-23 10:05:11 -0700

    Completed Unit Tests For All Scenarios

--- src/GameTest.java
+++ src/GameTest.java
@@ -16,4 +16,116 @@ public class GameTest {
     //assert
     assertEquals("no one", winner);
   }
+
+  @Test
+  public void DetermineWinner_Computer_Rock_vs_Human_Paper() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.ROCK;
+    var humanMove = Move.PAPER;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("you", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Rock_vs_Human_Scissors() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.ROCK;
+    var humanMove = Move.SCISSORS;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("computer", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Paper_vs_Human_Rock() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.PAPER;
+    var humanMove = Move.ROCK;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("computer", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Paper_vs_Human_Scissors() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.PAPER;
+    var humanMove = Move.SCISSORS;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("you", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Paper_vs_Human_Paper() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.PAPER;
+    var humanMove = Move.PAPER;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("no one", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Scissors_vs_Human_Rock() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.SCISSORS;
+    var humanMove = Move.ROCK;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("you", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Scissors_vs_Human_Paper() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.SCISSORS;
+    var humanMove = Move.PAPER;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("computer", winner);
+  }
+
+  @Test
+  public void DetermineWinner_Computer_Scissors_vs_Human_Scissors() {
+    //arrange
+    var game = new Game();
+    var computerMove = Move.SCISSORS;
+    var humanMove = Move.SCISSORS;
+
+    //act
+    var winner = game.DetermineWinner(computerMove, humanMove);
+
+    //assert
+    assertEquals("no one", winner);
+  }
 }
