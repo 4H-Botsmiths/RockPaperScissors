a45099d34a674fe0c5795b3c11ac3d323bae12c9
Kolton Bloom <bloomkd46@icloud.com>
2024-05-04 09:22:50 -0700

    Update game constructor to allow for human vs. computer gameplay

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Rock, Paper, Scissors!");
-        var game = new Game(new ComputerPlayer(), new ComputerPlayer());
+        var game = new Game(new HumanPlayer(), new ComputerPlayer());
         var result = game.Shoot();
         System.out.println(result);
     }
